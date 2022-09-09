46d5486acfc3766e24dc921489d9dfec5f6784c3
Kihun SONG <kihunbuzz@naver.com>
2022-09-09 22:42:46 +0900

    SWEA_2382_미생물격리

--- algorithm_study/09.12/SWEA_2382_미생물격리.py
+++ algorithm_study/09.12/SWEA_2382_미생물격리.py
@@ -1,29 +1,25 @@
 import sys
-import copy
+
 sys.stdin = open("09.12/미생물격리.txt", "r", encoding="UTF-8")
-# 2,3,7 케이스
+
 T = int(input())
 
 delta = [[0, 0], [-1, 0], [1, 0], [0, -1], [0, 1]]
 
 for test_case in range(1, T+1):
-    n, m, k = map(int,input().split())
-    data = [[0, 0, 0] * n for _ in range(n)]
+    n, m, k = map(int, input().split())
     virus = []
-    for index in range(k):
+    for _ in range(k):
         y, x, number, dir = map(int, input().split())
         virus.append([y, x, number, dir])
-    # for v in virus:
-    #         print(v)
-    # print("&&&&&&&&&&&&&&&&&&&&&")
     t = 0
     while t < m:
         cleanData = [[[0, 0, 0] for _ in range(n)] for _ in range(n)]
         for i in range(k):
-            y, x, number, dir = virus[i]
             # 합쳐진 값은 패스
-            if y == -1 and x == -1 and number == -1 and dir == -1:
+            if virus[i][0] == -1:
                 continue
+            y, x, number, dir = virus[i]
             # 다음 좌표로 이동
             nY = y + delta[dir][0]
             nX = x + delta[dir][1]
@@ -48,9 +44,9 @@ for test_case in range(1, T+1):
                 # 내 것이 더 크면 같은 데이터 찾아서 삭제, 인덱스 덮어쓰기
                 # cleanData에는 내 값을 비교용으로 남겨두고
                 # number에서 미생물 수 합치기
-                elif cleanData[nY][nX][0] < number:
-                    oldNumber = cleanData[nY][nX][0]
+                else:
                     deleteIndex = cleanData[nY][nX][2]
+                    oldNumber = virus[deleteIndex][2]
                     virus[deleteIndex] = [-1, -1, -1, -1]
                     cleanData[nY][nX][0] = number
                     cleanData[nY][nX][1] = dir
@@ -60,15 +56,12 @@ for test_case in range(1, T+1):
                 cleanData[nY][nX] = [number, dir, i]
             # virus 배열 최신화
             virus[i] = [nY, nX, number, dir]
-        # for v in virus:
-        #     print(v)
-        # print("********************")
         t += 1
+
+    # 결과값 출력
     result = 0
     for i in range(k):
-    #     print(virus[i])
-        if virus[i][3] > 0:
+        if virus[i][2] > 0:
             result += virus[i][2]
-    # print("****************************")
 
     print("#{} {}".format(test_case, result))
