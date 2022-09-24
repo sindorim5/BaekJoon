595c2421042147c5847dece9bc9d267416a46f86
Kihun SONG <kihunbuzz@naver.com>
2022-09-25 03:19:18 +0900

    벽돌깨기

--- algorithm_study/09.26/SWEA_5656_벽돌깨기.py
+++ algorithm_study/09.26/SWEA_5656_벽돌깨기.py
@@ -1,54 +1,45 @@
 import sys
-import copy
 from collections import deque
-# sys.stdin = open("09.26/벽돌깨기.txt", "r", encoding="UTF-8")
-sys.stdin = open("input.txt", "r", encoding="UTF-8")
-# 하 좌 우
-dy = [-1, 0, 0]
-dx = [0, -1, 1]
+
+sys.stdin = open("09.26/벽돌깨기.txt", "r", encoding="UTF-8")
+
+# 상 하 좌 우
+dy = [-1, 1, 0, 0]
+dx = [0, 0, -1, 1]
 
 T = int(input())
 
-def drop(idx, arr):
-    count = 0
-    startY = -1
-    for y in range(h):
-        if arr[y][idx] > 0:
-            startY = y
-            count = arr[y][idx]
-    if count > 0:
-        # 하 좌 우 0으로 만들기
-        q = deque()
-        q.append([startY, idx, count])
-        arr[startY][idx] = 0
-        while q:
-            nowY, nowX, popCount = q.popleft()
-            for d in range(3):
-                i = 0
-                while i < popCount-1:
-                    nY = nowY + dy[d]
-                    nX = nowX + dx[d]
-                    if 0 <= nY < h and 0 <= nX < w:
-                        if arr[nY][nX] == 0:
-                            continue
-                        q.append([nY, nX, arr[nY][nX]])
-                        arr[nY][nX] = 0
-                    else:
-                        break
-                    i += 1
-        # 0 정리하기
-        for x in range(w):
-            sortZero = []
-            for y in range(h-1, -1, -1):
-                if arr[y][x] > 0:
-                    sortZero.append(arr[y][x])
-                    arr[y][x] = 0
-            for i in range(len(sortZero)):
-                arr[h-1-i][x] = sortZero[i]
-    for asdf in arr:
-        print(arr)
 
-def block(depth, arr):
+def bfs(y, x, arr):
+    q = deque()
+    q.append([y, x, arr[y][x]])
+    arr[y][x] = 0
+    while q:
+        nowY, nowX, popCount = q.popleft()
+        for d in range(4):
+            for delta in range(1, popCount):
+                nY = nowY + delta * dy[d]
+                nX = nowX + delta * dx[d]
+                if 0 <= nY < h and 0 <= nX < w and arr[nY][nX] != 0:
+                    if arr[nY][nX] != 1:
+                        q.append([nY, nX, arr[nY][nX]])
+                    arr[nY][nX] = 0
+
+    # 0 정리하기
+    for a in range(w):
+        sortZero = []
+        for b in range(h-1, -1, -1):
+            if arr[b][a] > 0:
+                sortZero.append(arr[b][a])
+        for b in range(h-1, -1, -1):
+            if sortZero:
+                value = sortZero.pop(0)
+                arr[b][a] = value
+            else:
+                arr[b][a] = 0
+
+
+def dfs(depth, arr):
     global minBlock
     if depth == n:
         count = 0
@@ -58,14 +49,27 @@ def block(depth, arr):
                     count += 1
         minBlock = min(minBlock, count)
         return
-    tempArr = copy.deepcopy(arr)
-    for x in range(w):
-        block(depth, drop(x, tempArr))
 
-for test_case in range(1, 2):
+    for x in range(w):
+        # 새로 복사
+        tempArr = [arr[i][:] for i in range(h)]
+        tempY = 0
+
+        # 0이 아닌 y를 찾는다
+        while tempY < h and tempArr[tempY][x] == 0:
+            tempY += 1
+
+        # 찾았을 때
+        if tempY < h:
+            bfs(tempY, x, tempArr)
+
+        dfs(depth+1, tempArr)
+
+
+for test_case in range(1, T+1):
     n, w, h = map(int, input().split())
     matrix = [list(map(int, input().split())) for _ in range(h)]
     minBlock = 10**9
-    block(0, matrix)
+    dfs(0, matrix)
 
-    print(minBlock)
+    print("#{} {}".format(test_case, minBlock))
