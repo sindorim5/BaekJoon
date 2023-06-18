8d6af7c3061a202cf1876990b3e3371f53bf7266
Kihun SONG <kihunbuzz@naver.com>
2023-06-18 18:46:20 +0900

    톱니바퀴2.java ongoing

--- algorithm_java/src/main/java/Main.java
+++ algorithm_java/src/main/java/Main.java
@@ -1,17 +1,21 @@
-import programmers.lv3.베스트앨범;
+import baekjoon.gold.톱니바퀴2;
+import programmers.lv1.모의고사;
+import programmers.lv2.소수찾기;
 
+import java.io.IOException;
 import java.util.Arrays;
 
 public class Main {
 
-    public static void main(String[] args) {
-        베스트앨범 BestAlbum = new 베스트앨범();
-
-        int[] sol = BestAlbum.solution(
-                new String[]{"classic", "pop", "classic", "classic", "pop"},
-                new int[]{500, 600, 150, 800, 2500}
-        );
-        System.out.println(Arrays.toString(sol));
+    public static void main(String[] args) throws IOException {
+//        소수찾기 temp = new 소수찾기();
+//
+//        int sol = temp.solution(
+//            "011"
+//        );
+//        System.out.println(sol);
+        톱니바퀴2 temp = new 톱니바퀴2();
+        temp.main(args);
     }
 
 }
